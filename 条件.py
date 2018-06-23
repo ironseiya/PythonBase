26cb35b00ffa5971817b23355c40a674098d9272
ironseiya <seiya.liulin@gmail.com>
2018-06-23 21:21:23 +0800

    first commit

---
+++ 条件.py
@@ -0,0 +1,18 @@
+# _*_ coding: utf-8 _*_
+
+print("hello world")
+
+if 1 == 1:
+    print("欢迎来到Python的世界")
+else:
+    print("算了你还是不适合学习Python")
+
+inp = input("请输入你的爱好：")
+
+if inp == "篮球":
+    print("开始打篮球")
+elif inp == "足球":
+    print("开始踢足球")
+else:
+    print("还是睡觉吧")
+
